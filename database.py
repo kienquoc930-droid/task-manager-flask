4e5c5a26b377cb916fb30960be460d11de211ece
Phuong An <emailcuaban@gmail.com>
2026-02-01 20:18:37 +0700

    Auto create tasks table

--- database.py
+++ database.py
@@ -1,43 +1,12 @@
 import sqlite3
 
-DB_NAME = "tasks.db"
-
-def get_db():
-    conn = sqlite3.connect(DB_NAME)
-    conn.row_factory = sqlite3.Row
+def get_connection():
+    conn = sqlite3.connect("tasks.db")
+    conn.execute("""
+        CREATE TABLE IF NOT EXISTS tasks (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            title TEXT NOT NULL
+        )
+    """)
+    conn.commit()
     return conn
-
-def get_all_tasks():
-    conn = get_db()
-    tasks = conn.execute("SELECT * FROM tasks").fetchall()
-    conn.close()
-    return tasks
-
-def add_task(name):
-    conn = get_db()
-    conn.execute("INSERT INTO tasks (name) VALUES (?)", (name,))
-    conn.commit()
-    conn.close()
-
-def delete_task(task_id):
-    conn = get_db()
-    conn.execute("DELETE FROM tasks WHERE id = ?", (task_id,))
-    conn.commit()
-    conn.close()
-def get_task(task_id):
-    conn = get_db()
-    task = conn.execute(
-        "SELECT * FROM tasks WHERE id = ?", (task_id,)
-    ).fetchone()
-    conn.close()
-    return task
-
-
-def update_task(task_id, name):
-    conn = get_db()
-    conn.execute(
-        "UPDATE tasks SET name = ? WHERE id = ?",
-        (name, task_id)
-    )
-    conn.commit()
-    conn.close()
